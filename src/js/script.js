f88c59a911902a3b8a4233e9f750981b4fa3da62
lauramirilli <lcardinmirilli@gmail.com>
2026-05-07 09:36:27 -0300

    feat: adicionando menu hamburguer

--- src/js/script.js
+++ src/js/script.js
@@ -14,3 +14,15 @@ window.onload = function(){
 function trocar(cor) {
     document.body.style.background = cor;
 }
+
+//MENU BURGER
+document.addEventListener('DOMContentLoaded',()=>{
+    const menuIcone = document.getElementById("menu-icone");
+    const navMenu = document.querySelector("nav");
+    if (menuIcone && navMenu){
+        menuIcone.onclick = ()=>{
+            navMenu.classList.toggle("active");
+            menuIcone.classList.toggle("open");
+        }
+    }
+})
